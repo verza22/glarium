cf1f0220d33e9e97497a5d3b4c959959f0cd8ccb
lzurita <32106691+lzurita@users.noreply.github.com>
2019-12-22 14:43:27 -0500

    Seleccionador de ciudades

--- resources/game/components/layout/navigation/Cities.vue
+++ resources/game/components/layout/navigation/Cities.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="flex-1 navigation-box">
-        <div class="navigation" :title="$t('options.navigation.showCities')">
+    <div class="flex-1 box position-relative">
+        <div class="nav nav-selected" :title="$t('options.navigation.showCities')" @click='showCities'>
             <div class="city">{{getSelected()}}</div>
+        </div>
+        <div class="nav-other-container" v-if="show">
+            <div class="nav nav-other" v-for='(city,i) in getOther()' :key='i'>
+                <div class="city">{{`[${city.x}:${city.y}] ${city.name}`}}</div>
+            </div>
+            <div class="nav-footer"></div>
         </div>
     </div>
 </template>
@@ -19,6 +25,9 @@ export default {
         }
     },
     methods:{
+        showCities(){
+            this.show = !this.show;
+        },
         getCities(){
             axios("city/getCities")
             .then(res => {
@@ -33,6 +42,11 @@ export default {
                 }else{
                     x.selected = false;
                 }
+            })
+        },
+        getOther(){
+            return this.data.filter(x =>{
+                return !x.selected;
             })
         },
         getSelected(){
@@ -61,16 +75,38 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.navigation-box{
-    padding: 1px 0px 0px 4px;
+.box{
+    user-select: none;
 }
-.navigation{
-    background-image: url('~Img/icon/navigation.jpg');
+.nav{
     height: 23px;
     background-size: cover;
     cursor: pointer;
 }
-.navigation:hover{
+.nav-selected{
+    background-image: url('~Img/icon/navigation-selected.jpg');
+}
+.nav-other-container{
+    position: absolute;
+    width: 100%;
+}
+.nav-other{
+    background-image: url('~Img/icon/navigation-other.jpg');
+    background-position-y: 28px;
+}
+.nav-other:hover{
+    background-position-y: 52px;
+}
+.footer{
+    background-image: url('~Img/icon/navigation-other.jpg');
+    background-position-y: bottom;
+    height: 28px;
+}
+.nav-footer{
+    background-image: url('~Img/icon/navigation-footer.jpg');
+    height: 5px;
+}
+.nav-selected:hover{
     background-position-y: -23px;
 }
 .city{

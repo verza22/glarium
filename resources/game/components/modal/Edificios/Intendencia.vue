874a631cb083dc93297fc48b74e04f958416335e
Luis Zurita <zuritabal@gmail.com>
2019-12-31 12:40:28 -0500

    Trabajando en la intendencia

--- resources/game/components/modal/Edificios/Intendencia.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>Intendencia</div>
-</template>
-
-<script>
-export default {
-    name:'Intendencia'
-}
-</script>
---
+++ resources/game/components/modal/Edificios/Intendencia/Intendencia.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="box pt-0">
+        <div class="gtitle text-center">Colonia Polis</div>
+        <Informacion></Informacion>
+    </div>
+</template>
+
+<script>
+import Informacion from 'Components/modal/Edificios/Intendencia/Informacion.vue'
+
+export default {
+    name:'Intendencia',
+    components:{
+        Informacion
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .box{
+        font-size: 0.83rem;
+        line-height: 0.83rem;
+    }
+</style>

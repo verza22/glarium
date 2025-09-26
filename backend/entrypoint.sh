f8e61e71d676b736503329e49bc17e172866540f
Luis Zurita <46327452+verza22@users.noreply.github.com>
2025-09-26 11:17:49 +0200

    docker changes

---
+++ backend/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# entrypoint.sh
+
+# Wait to docker enable the DB
+until nc -z db 3306; do
+  echo "Waiting for database..."
+  sleep 1
+done
+
+echo "Database ready! Running migrations and seed..."
+npx prisma migrate deploy --schema=src/dataAccess/prisma/schema.prisma
+npx prisma generate --schema=src/dataAccess/prisma/schema.prisma
+npx ts-node src/dataAccess/prisma/seed.ts
+
+# Exec backend
+exec node dist/index.js
